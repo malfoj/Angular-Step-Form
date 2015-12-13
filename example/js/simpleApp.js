29fa86d1540ceb38180a982700a0ed54fd52657a
malfoj <123qwe>
2015-12-13 11:59:51 +0100

    add example + code example

--- example/js/simpleApp.js
+++ example/js/simpleApp.js
@@ -3,7 +3,7 @@
 	angular.module("example", [])
 	
 	.controller("exampleController", function($scope){
-	$scope.welcome = "Welcome in Example Controller Scope";
+	$scope.welcome = "Step form example";
 	$scope.data = [
 						{
                                         type: 'i',
